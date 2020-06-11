6dacb56a5a2c086099957e1d7b3a184b182afc47
Lee Sutton <leesutton1@gmail.com>
2020-06-11 16:54:01 -0700

    fix auth tests

--- test/readit_later/account_test.exs
+++ test/readit_later/account_test.exs
@@ -58,11 +58,11 @@ defmodule ReaditLater.AccountTest do
     end
 
     test "validates email and password when given" do
-      {:error, changeset} = Account.register_user(%{email: "not valid", password: "not valid"})
+      {:error, changeset} = Account.register_user(%{email: "not valid", password: "123456"})
 
       assert %{
                email: ["must have the @ sign and no spaces"],
-               password: ["should be at least 12 character(s)"]
+               password: ["should be at least 8 character(s)"]
              } = errors_on(changeset)
     end
 
@@ -234,12 +234,12 @@ defmodule ReaditLater.AccountTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Account.update_user_password(user, valid_user_password(), %{
-          password: "not valid",
+          password: "short",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 8 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
@@ -443,12 +443,12 @@ defmodule ReaditLater.AccountTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Account.reset_user_password(user, %{
-          password: "not valid",
+          password: "short",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 8 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
